29dd2151685be7cde1cf268ced933d9df3b89f47
ddhaha <ddhecnu@gmail.com>
2021-01-10 20:19:22 +0800

    update

---
+++ neural_network/neuralnetwork.py
@@ -0,0 +1,23 @@
+import numpy as np
+# import torch
+
+class NeuralNetwork:
+    def __init__(self, layer_sizes):
+        # layer_sizes = (2,3,5,2)
+        w_shapes = [(a,b) for a,b in zip(layer_sizes[1:], layer_sizes[:-1])]
+        self.weights = [np.random.standard_normal(s) for s in w_shapes]
+        self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]]
+        # print(w_shapes)
+        # for w in weights:
+        #     print(w,'\n')
+    
+    def predict(self, a):
+        for w,b in zip(self.weights, self.biases):
+            z = np.matmul(w,a) + b
+            print(z[0])
+            a = self.activation(np.matmul(w,a) + b)
+        return a 
+
+    @staticmethod
+    def activation(x):
+        return 1/(1+np.exp(-x))
---
+++ neural_network/program.py
@@ -0,0 +1,12 @@
+import neuralnetwork as nn
+import numpy as np
+
+layer_sizes = (3,5,10)
+x = np.ones((layer_sizes[0],1))
+
+net = nn.NeuralNetwork(layer_sizes)
+prediction = net.predict(x)
+
+print(x)
+
+print(prediction)
